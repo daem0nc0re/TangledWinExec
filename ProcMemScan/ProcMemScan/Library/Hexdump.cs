91b70b7cb9270ec22dc372ec89f118da0d71643d
daem0nc0re <daem0nc0re@outlook.com>
2023-05-14 06:37:00 +0200

    Updated HexDump class

--- ProcMemScan/ProcMemScan/Library/Hexdump.cs
+++ ProcMemScan/ProcMemScan/Library/Hexdump.cs
@@ -45,85 +45,63 @@ namespace ProcMemScan.Library
         }
 
 
-        public static void Dump(
-            IntPtr pBufferToRead,
-            IntPtr pBaseAddress,
-            uint nRange,
-            int nIndentCount)
+        public static void Dump(IntPtr pBufferToRead, IntPtr pBaseAddress, uint nRange, int nIndentCount)
         {
-            IntPtr address;
-            IntPtr pByteToRead;
-            byte readByte;
             string addressFormat;
             string headFormat;
             string lineFormat;
-            string indent = new string(' ', nIndentCount * 4);
-            var hexBuffer = new StringBuilder();
-            var charBuffer = new StringBuilder();
+            var hexBuilder = new StringBuilder();
+            var charBuilder = new StringBuilder();
+            var outputBuilder = new StringBuilder();
 
             if (pBaseAddress == new IntPtr(-1))
             {
                 pBaseAddress = IntPtr.Zero;
                 addressFormat = "X8";
-                headFormat = string.Format("{{0}}{{1,{0}}}   {{2,-47}}\n", 8);
-                lineFormat = string.Format("{{0}}{{1,{0}}} | {{2,-47}} | {{3}}", 8);
+                headFormat = string.Format("{{0,{0}}}   {{1,-47}}\n\n", 8 + (nIndentCount * 4));
+                lineFormat = string.Format("{{0,{0}}} | {{1,-47}} | {{2}}\n", 8 + (nIndentCount * 4));
             }
             else
             {
                 addressFormat = (IntPtr.Size == 8) ? "X16" : "X8";
-                headFormat = string.Format("{{0}}{{1,{0}}}   {{2,-47}}\n", (IntPtr.Size * 2));
-                lineFormat = string.Format("{{0}}{{1,{0}}} | {{2,-47}} | {{3}}", (IntPtr.Size * 2));
+                headFormat = string.Format("{{0,{0}}}   {{1,-47}}\n\n", (IntPtr.Size * 2) + (nIndentCount * 4));
+                lineFormat = string.Format("{{0,{0}}} | {{1,-47}} | {{2}}\n", (IntPtr.Size * 2) + (nIndentCount * 4));
             }
 
             if (nRange > 0)
-                Console.WriteLine(headFormat, indent, string.Empty, "00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F");
-            else
-                return;
-
-            for (var idx = 0; idx < nRange; idx++)
             {
-                address = new IntPtr(pBaseAddress.ToInt64() + (idx & (~0x0Fu)));
-                pByteToRead = new IntPtr(pBufferToRead.ToInt64() + idx);
-                readByte = Marshal.ReadByte(pByteToRead);
-                hexBuffer.Append(string.Format("{0}", readByte.ToString("X2")));
+                outputBuilder.Append(string.Format(headFormat, string.Empty, "00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F"));
 
-                if (IsPrintable((char)readByte))
-                    charBuffer.Append((char)readByte);
-                else
-                    charBuffer.Append(".");
+                for (var idx = 0; idx < nRange; idx++)
+                {
+                    var address = pBaseAddress.ToInt64() + (idx & (~0x0Fu));
+                    var readByte = Marshal.ReadByte(pBufferToRead, idx);
+                    hexBuilder.Append(readByte.ToString("X2"));
+                    charBuilder.Append(IsPrintable((char)readByte) ? (char)readByte : '.');
 
-                if (((idx + 1) % 8 == 0) && ((idx + 1) % 16 != 0) && ((idx + 1) != nRange))
-                {
-                    hexBuffer.Append("-");
-                    charBuffer.Append(" ");
-                }
-                else if (((idx + 1) % 16 != 0) && ((idx + 1) != nRange))
-                {
-                    hexBuffer.Append(" ");
+                    if ((idx % 16 == 15) || (idx == (nRange - 1)))
+                    {
+                        outputBuilder.Append(string.Format(
+                            lineFormat,
+                            address.ToString(addressFormat),
+                            hexBuilder.ToString(),
+                            charBuilder.ToString()));
+                        hexBuilder.Clear();
+                        charBuilder.Clear();
+                    }
+                    else if ((idx % 16 == 7) && (idx != (nRange - 1)))
+                    {
+                        hexBuilder.Append("-");
+                        charBuilder.Append(" ");
+                    }
+                    else
+                    {
+                        hexBuilder.Append(" ");
+                    }
                 }
 
-                if ((idx + 1) % 16 == 0)
-                {
-                    Console.WriteLine(
-                        lineFormat,
-                        indent,
-                        address.ToString(addressFormat),
-                        hexBuffer,
-                        charBuffer);
-                    hexBuffer.Clear();
-                    charBuffer.Clear();
-                }
-                else if ((idx + 1) == nRange)
-                {
-                    Console.WriteLine(
-                        lineFormat,
-                        indent,
-                        address.ToString(addressFormat),
-                        hexBuffer,
-                        charBuffer);
-                    hexBuffer.Clear();
-                    charBuffer.Clear();
-                }
+                Console.WriteLine(outputBuilder.ToString());
+                outputBuilder.Clear();
             }
         }
 
