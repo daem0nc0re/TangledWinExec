eb4a6c4064e7d38040ac2b5eabda1bd1d0ce3e9c
daem0nc0re <daem0nc0re@outlook.com>
2023-09-18 16:48:41 +0900

    Removed unused code from HandleScanner

--- Misc/HandleScanner/HandleScanner/Library/Helpers.cs
+++ Misc/HandleScanner/HandleScanner/Library/Helpers.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
-using System.Xml.Linq;
 using HandleScanner.Interop;
 
 namespace HandleScanner.Library
